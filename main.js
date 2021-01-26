c29f9523187c7b6e7f050e26a375fcf55e0bc67b
Jayden Sujun Song <songjayden@gmail.com>
2021-01-27 00:15:10 +1300

    menu bar select by page

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-  console.log(navbarHeight);
+  // console.log(window.scrollY);
+  // console.log(navbarHeight);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
   } else {
@@ -24,6 +24,7 @@ navbarMenu.addEventListener("click", (event) => {
   }
   navbarMenu.classList.remove("open");
   scrollIntoView(link);
+  selectNavItem(target);
 });
 //Navbar toggle button for small screen
 const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
@@ -92,3 +93,55 @@ function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
 }
+
+// get section id
+//Observe all sectons with using IntersectionObserver
+//menu item active on showing section 
+
+const sectionIds = ['#home','#about','#skills','#work','#testimonials','#contact',];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => 
+  document.querySelector(`[data-link="${id}"]`));
+
+  let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove('active');
+  selectedNavItem = selected;
+  selectedNavItem.classList.add('active');
+} 
+
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.3,
+
+};
+const observerCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+
+      // scroll by down
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      }else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel', ()=> {
+  if ( window.scrollY === 0 ) {
+    selectedNavIndex = 0;
+  } else if (window.scrollY+ window.innerHeight === document.body.clientHeight){
+    selectedNavIndex = navItems.length - 1;
+
+  }
+selectNavItem(navItems[selectedNavIndex])});
